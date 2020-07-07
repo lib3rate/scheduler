3842ba49f447b1d89579f5e83e44a85c18a22ee1
Dmitry <dmitry.petrov0918@gmail.com>
2020-07-07 18:24:09 -0400

    Added a general Appointment story

--- stories/index.js
+++ stories/index.js
@@ -128,4 +128,9 @@ storiesOf("InterviewerList", module)
     />
   ));
 
-  
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);

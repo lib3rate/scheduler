86cb70efa872a7d19da6b86b6d50a38dbfc16a33
Dmitry <dmitry.petrov0918@gmail.com>
2020-07-07 18:04:02 -0400

    Added a Day list and Interviewer list functionality

---
+++ src/components/DayListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classnames from 'classnames/bind';
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const formatSpots = (props) => {
+    return props.spots === 0 ? "no spots remaining" :
+    props.spots === 1 ? "1 spot remaining": 
+    `${props.spots} spots remaining`
+  }
+
+  return (
+    <li className={dayClass} onClick={props.setDay}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(value => {
+    return (
+      <InterviewerListItem
+        key={value.id}
+        name={value.name}
+        avatar={value.avatar}
+        selected={value.id === props.value}
+        setInterviewer={event => props.onChange(value.id)}
+      />
+    );
+  })
+  
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  )
+}

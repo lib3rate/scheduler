6761ecdcd0bc129650d64b3b6599c528b85b3d97
Dmitry <dmitry.petrov0918@gmail.com>
2020-07-07 21:11:25 -0400

    Added an Form component and story to the Appointment component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,18 +3,19 @@ import InterviewerList from "../InterviewerList";
 import Button from "../Button";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={name}
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-            */
+            onChange={event => setName(event.target.value)}
           />
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />

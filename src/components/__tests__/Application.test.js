58ac6ff01eb7f4be4da37f03faf145aa65042449
Dmitry <dmitry.petrov0918@gmail.com>
2020-07-15 18:37:51 -0400

    Removed unused code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -28,74 +28,68 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
-  // it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  //   const { container, debug } = render(<Application />);
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
   
-  //   await waitForElement(() => getByText(container, "Archie Cohen"));
+    await waitForElement(() => getByText(container, "Archie Cohen"));
   
-  //   const appointments = getAllByTestId(container, "appointment");
-  //   const appointment = appointments[0];
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
   
-  //   fireEvent.click(getByAltText(appointment, "Add"));
-  //   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-  //     target: { value: "Lydia Miller-Jones" }
-  //   });
-  //   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-  //   fireEvent.click(getByText(appointment, "Save"));
+    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
   
-  //   expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
-  //   await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
-  //   const day = getAllByTestId(container, "day").find(day =>
-  //     queryByText(day, "Monday")
-  //   );
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
     
-  //   expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
-  // });
+    expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
+  });
 
-  // it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-  //   const { container, debug } = render(<Application />);
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container, debug } = render(<Application />);
 
-  //   await waitForElement(() => getByText(container, "Archie Cohen"));
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
-  //   const appointment = getAllByTestId(container, "appointment").find(
-  //     appointment => queryByText(appointment, "Archie Cohen")
-  //   );
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
 
-  //   fireEvent.click(getByAltText(appointment, "Delete"));
+    fireEvent.click(getByAltText(appointment, "Delete"));
     
-  //   expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
+    expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
 
-  //   fireEvent.click(getByText(appointment, "Confirm"));
+    fireEvent.click(getByText(appointment, "Confirm"));
     
-  //   expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
     
-  //   await waitForElement(() => getByAltText(appointment, "Add"));
+    await waitForElement(() => getByAltText(appointment, "Add"));
     
-  //   const day = getAllByTestId(container, "day").find(day =>
-  //     queryByText(day, "Monday")
-  //     );
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+      );
 
-  //   expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
-  // });
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+  });
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
     const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
-    
-    const day = getAllByTestId(container, "day").find(day =>
-      queryByText(day, "Monday")
-      );
-    // console.log(prettyDOM(day));
 
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
     
     fireEvent.click(getByAltText(appointment, "Edit"));
-    // console.log(prettyDOM(appointment));
     fireEvent.change(getByTestId(appointment, "student-name-input"), {
       target: { value: "Lydia Miller-Jones" }
     });
@@ -104,13 +98,10 @@ describe("Application", () => {
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
     
     await waitForElement(() => getByText(container, "Lydia Miller-Jones"));
-    // console.log(prettyDOM(appointment));
     
-    // const day = getAllByTestId(container, "day").find(day =>
-    //   queryByText(day, "Monday")
-    //   );
-
-    // console.log(prettyDOM(day));
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+      );
 
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,16 +11,6 @@ export default function useApplicationData() {
 
   const setDay = day => setState({ ...state, day });
 
-  // const spotsRemaining = interviewId => {
-  //   // 5 - 
-  //   const correctDay = findDay(interviewId);
-  //   const daySpots = correctDay.spots;
-
-  //   // for (let appointmentId in state.days[day].appointments)
-
-  //   return daySpots;
-  // }
-
   const findDay = appointmentId => {
     for (let oneDay of state.days) {
       if (oneDay.appointments.includes(appointmentId)) {
@@ -31,21 +21,10 @@ export default function useApplicationData() {
 
   function bookInterview(id, interview, edit = false) {
     const currentDay = findDay(id);
+
     if (edit === false) {
       currentDay.spots -= 1;
     };
-    // const updatedSpots = currentDay.spots -= 1;
-
-    // const day = {
-    //   ...state.days[currentDay.id - 1],
-    //   spots: updatedSpots
-    // }
-
-    // const days = {
-    //   ...state.days,
-    //   [currentDay.id]: day
-    // }
-    // console.log(days);
 
     const appointment = {
       ...state.appointments[id],
@@ -61,27 +40,15 @@ export default function useApplicationData() {
         .then(() => {
           setState({
             ...state,
-            // days,
             appointments
           })
         })
-        // .catch(error => console.log(error))
     );
   };
 
   function cancelInterview(id, interview) {
     const currentDay = findDay(id);
     const updatedSpots = currentDay.spots += 1;
-
-    // const day = {
-    //   ...state.days[currentDay.id - 1],
-    //   spots: updatedSpots
-    // }
-
-    // const days = {
-    //   ...state.days,
-    //   [currentDay.id]: day
-    // }
 
     const appointment = {
       ...state.appointments[id],
@@ -96,11 +63,9 @@ export default function useApplicationData() {
         .then(() => {
           setState({
             ...state,
-            // days,
             appointments
           })
         })
-        // .catch(error => console.log(`There was an error: ${error}`))
     );
   };
 
